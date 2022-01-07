cc085380dad95f2249c5e1df47306f5a6b06d1a9
Nasim JR <nasimuddin0994@gmail.com>
2022-01-07 21:55:57 +0600

    Digital Clock

---
+++ app.js
@@ -0,0 +1,34 @@
+const clock = () => {
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let formate = "AM";
+
+    hours = hours === 0 ? 12 : hours;
+    if(hours > 10){
+        hours = hours - 12;
+        formate = "pm";
+    }
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let finalClock = `${hours}:${minutes}:${seconds}`
+    document.getElementById('clock').innerText = finalClock;
+    document.getElementById('formate').innerText = formate;
+
+}
+clock();
+setInterval(clock, 1000);
+
+
+
+const events = document.querySelectorAll('.fullYear');
+    events.forEach(e => {
+        const day = dayjs(e.dataset.date).format("dddd, D-MMM-YYYY");
+        document.getElementById('day').innerText = day;
+    });
+
+    
